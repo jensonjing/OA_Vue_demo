e5eef8cfc7102c30de4d0286e53bd4348223d865
e <jensonjing@gmail.com>
2018-12-11 15:54:47 +0800

    this is a oa demo for vue

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+*{
+  margin: 0;
+  padding: 0;
+}
+.el-submenu .el-menu-item{
+  padding-left:0!important;
+  padding-right:0!important;
+  min-width: 150px;
+}
+</style>
